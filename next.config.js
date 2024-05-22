821b2ba702b07468efcb2b533b9da0759419d6d5
hiumesh <daniumeshchandra.se@gmail.com>
2024-05-22 08:42:11 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["picsum.photos"],
+    remotePatterns: [{
+      hostname: 'picsum.photos',
+    }],
   },
 };
 
